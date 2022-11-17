35d0d1b2c93f8a7e07b573cd9a88b7cf4067aabc
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-11-17 19:59:21 +0300

    end HW9

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,24 +1,25 @@
-import {UserType} from '../HW8'
+import {UserType} from '../HW8';
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): any => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
             if (action.payload === 'up') {
-                state.sort((a, b) => a.name > b.name ? 1 : -1)
+                state.sort((a, b) => a.name > b.name ? 1 : -1);
             }
             if (action.payload === 'down') {
                 state.sort((a, b) => a.name < b.name ? 1 : -1);
             }
-            return [...state]
+
+            return [...state]; // need to fix
         }
         case 'check': {
-            return state.filter(el => el.age > action.payload)
+            return state.filter(el => el.age > action.payload);
         }
         default:
-            return state
+            return state;
     }
-}
+};
