d7fb6ebe22bb97ad09e363c2b9e7e9e3dcf46ce0
Zheniqq <yuheni.mikhaylov@gmail.com>
2022-12-05 18:51:40 +0300

    add css for HW11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,11 +5,35 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+                color: 'cadetblue',
+                '& .MuiSlider-rail': {
+                    color: "#8B8B8B",
+                },
+
+                // '& #hw11-single-slider:first-child': {
+                //   width: '185px'
+                // },
+                '& .MuiSlider-thumb': {
+                    color: '#fff',
+
+                    border: '2px solid cadetblue',
+                },
+                '& .MuiSlider-thumb:after': {
+                    width: '6px',
+                    height: '6px',
+                    top: '50%',
+                    left: '50%',
+                    backgroundColor: 'cadetblue',
+                }
+
             }}
+            step={1}
+            min={1}
+            max={100}
+            getAriaLabel={() => 'Temperature range'}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
-    )
+    );
 }
 
 export default SuperRange
